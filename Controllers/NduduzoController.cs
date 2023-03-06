deec19729d7371a76c1c3931a1a3392594a3dab6
Nduduzo Shabalala <54292980+volubleogre664@users.noreply.github.com>
2023-03-06 12:34:22 +0200

    Added new page along with it's index controller

---
+++ Controllers/NduduzoController.cs
@@ -0,0 +1,21 @@
+﻿using FirstMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstMVCProject.Controllers
+{
+    public class NduduzoController : Controller
+    {
+        private readonly ILogger<NduduzoController> _logger;
+
+        public NduduzoController(ILogger<NduduzoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
