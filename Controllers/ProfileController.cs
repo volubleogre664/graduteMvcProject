1abf104ec43c19ab3672b66d2667af3753dd8f3e
Nduduzo Shabalala <54292980+volubleogre664@users.noreply.github.com>
2023-03-07 08:03:39 +0200

    Implemented the profile pagea and the model

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCProject.Models;
+
+namespace FirstMVCProject.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            var profile = new ProfileViewModel("Nduduzo", "Shabalala", DateTime.Now, 32, "Russia", "Moscow");
+
+            return View(profile);
+        }
+    }
+}
